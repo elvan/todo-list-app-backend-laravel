129303696481b280eb39c87d9dd9890e723df2be
Elvan Hidayat <elvanhd@gmail.com>
2023-07-02 08:49:44 +0700

    registering a user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
@@ -19,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', RegisterController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
